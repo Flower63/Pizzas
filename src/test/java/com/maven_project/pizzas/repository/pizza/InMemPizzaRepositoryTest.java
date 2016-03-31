155abbfe9b5e116c34b90e68e979c235a5fbab59
Denys Zvarych <Denys_Zvarych@epam.com>
2016-03-31 19:07:00 +0300

    Test adjustments

---
+++ src/test/java/com/maven_project/pizzas/repository/pizza/InMemPizzaRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.maven_project.pizzas.repository.pizza;
+
+import junit.framework.TestCase;
+
+public class InMemPizzaRepositoryTest extends TestCase {
+
+	public void testGetPizzaByID() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/java/com/maven_project/pizzas/service/order/SimpleOrderServiceTest.java
+++ src/test/java/com/maven_project/pizzas/service/order/SimpleOrderServiceTest.java
@@ -2,6 +2,7 @@ package com.maven_project.pizzas.service.order;
 
 import static org.mockito.Mockito.*;
 
+import com.maven_project.pizzas.domain.Customer;
 import com.maven_project.pizzas.domain.Order;
 import com.maven_project.pizzas.repository.order.InMemOrderRepository;
 import com.maven_project.pizzas.repository.order.OrderRepository;
@@ -23,7 +24,7 @@ public class SimpleOrderServiceTest extends TestCase {
 		
 		OrderService orderService = new SimpleOrderService(orderRepository, pizzaRepository, discountServiceImplenementation);
 		
-		orderService.placeNewOrder(null, 1, 2, 3);
+		Order order = orderService.placeNewOrder(null, 1, 2, 3);
 		
 		verify(pizzaRepository).getPizzaByID(1);
 		verify(pizzaRepository).getPizzaByID(2);
@@ -31,10 +32,33 @@ public class SimpleOrderServiceTest extends TestCase {
 		
 		verify(orderRepository).saveOrder(any(Order.class));
 		
+		assertTrue(order.getPizzas().size() == 3);
 	}
 
 	public void testProceedOrder() {
+		OrderRepository orderRepository = mock(OrderRepository.class);
+		
+		PizzaRepository pizzaRepository = mock(PizzaRepository.class);
+		
+		DiscountServiceImplenementation discountServiceImplenementation = mock(DiscountServiceImplenementation.class);
+		
+		OrderService orderService = new SimpleOrderService(orderRepository, pizzaRepository, discountServiceImplenementation);
+		
+		Order order = orderService.placeNewOrder(new Customer(1, "John", null), 1, 2, 3);
 
+		assertTrue(order.getState() == Order.State.NEW);
+		
+		order.proceedOrder();
+		
+		assertTrue(order.getState() == Order.State.IN_PROGRESS);
+		
+		order.proceedOrder();
+		
+		assertTrue(order.getState() == Order.State.DONE);
+		
+		order.proceedOrder();
+		
+		assertTrue(order.getState() == Order.State.DONE);
 	}
 
 	public void testCancelOrder() {
