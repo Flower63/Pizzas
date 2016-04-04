ac0c9e4ab32c77964c910d556d452c32b325889d
Antonio <Antonio@10.17.163.129>
2016-04-04 16:01:50 +0300

    work 04.04

--- src/main/java/com/maven_project/pizzas/ifrastructure/JavaConfigApplicationContext.java
+++ src/main/java/com/maven_project/pizzas/ifrastructure/JavaConfigApplicationContext.java
@@ -1,6 +1,7 @@
 package com.maven_project.pizzas.ifrastructure;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,36 +11,102 @@ public class JavaConfigApplicationContext implements ApplicationContext {
 	private final Map<String, Object> context = new HashMap<>();
 	
 	@Override
-	public Object getBean(String bean) throws Exception {
-		Object cache = context.get(bean);
+	public Object getBean(String beanName) throws Exception {
+		Object bean = context.get(beanName);
 		
-		if (cache != null) {
-			return cache;
+		if (bean != null) {
+			return bean;
 		}
 		
-		Class<?> clazz = config.getImpl(bean);
+		Class<?> clazz = config.getImpl(beanName);
 		
-		Constructor<?> constructor = clazz.getConstructors()[0];
-		Class<?>[] params = constructor.getParameterTypes();
+		BeanBuilder builder = new BeanBuilder(clazz, this);
+		builder.createBean();
+		builder.createBeanProxy(); //@BanchMark annotation -Method, (active = true/false)
+		builder.callPostConstructMethod();
+		builder.callInitMethod(); // call init() method
+		bean = builder.build();
+
+		context.put(beanName, bean);
 		
-		if (params.length == 0) {
-			cache = clazz.newInstance();
-		} else {
+		return bean;
+	}
+
+	static class BeanBuilder {
+		private final Class<?> clazz;
+		private Object bean;
+		private ApplicationContext context;
+
+		public BeanBuilder(Class<?> clazz, ApplicationContext context) {
+			this.clazz = clazz;
+			this.context = context;
+		}
+
+		public Object build() {
+			return bean;
+		}
+
+		public void callInitMethod() throws Exception {
+			Method[] methods = clazz.getMethods();
+			
+			for (Method m : methods) {
+				if (m.getAnnotation(PostConstruct.class) != null) {
+					m.invoke(bean);
+				}
+			}
+		}
+
+		public void callPostConstructMethod() throws Exception {
+			Method[] methods = clazz.getMethods();
+			
+			for (Method m : methods) {
+				if (m.getAnnotation(PostConstruct.class) != null) {
+					m.invoke(bean);
+				}
+			}
+		}
+
+		public void createBeanProxy() {
+			// TODO Auto-generated method stub
+			// Proxy.newProxyInstance
+		}
+
+		public void createBean() throws Exception {
+			Constructor<?> constructor = clazz.getConstructors()[0];
+			Class<?>[] params = constructor.getParameterTypes();
+			
+			if (params.length == 0) {
+				bean = clazz.newInstance();
+			} else {
+				bean = createNewInstanceWithParams(constructor, params);
+			}
+			
+		}
+		
+		private Object createNewInstanceWithParams(Constructor<?> constructor, Class<?>[] params)
+				throws Exception {
+			Object cache;
 			Object[] paramBeans = new Object[params.length];
 			
 			for (int i = 0; i < params.length; i++) {
-				String paramName;
-				String name = params[i].getSimpleName();
-				paramName = Character.toLowerCase(name.charAt(0)) + name.substring(1);
-				paramBeans[i] = getBean(paramName);
+				String paramName = getBeanNameByType(params, paramBeans, i);
+				paramBeans[i] = context.getBean(paramName);
 			}
 			
 			cache = constructor.newInstance(paramBeans);
+			return cache;
+		}
+		
+		private String getBeanNameByType(Class<?>[] params, Object[] paramBeans, int i) throws Exception {
+			String name = params[i].getSimpleName();
+			String paramName = changeFirstLetterToLowerCase(name);
+			return paramName;
 		}
 
-		context.put(bean, cache);
+		private String changeFirstLetterToLowerCase(String name) {
+			String paramName = Character.toLowerCase(name.charAt(0)) + name.substring(1);
+			return paramName;
+		}
 		
-		return cache;
 	}
-
 }
--- src/main/java/com/maven_project/pizzas/repository/pizza/InMemPizzaRepository.java
+++ src/main/java/com/maven_project/pizzas/repository/pizza/InMemPizzaRepository.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import com.maven_project.pizzas.Pizza;
 import com.maven_project.pizzas.Pizza.Type;
+import com.maven_project.pizzas.ifrastructure.PostConstruct;
 
 public class InMemPizzaRepository implements PizzaRepository {
-	private List<Pizza> pizzasList = new ArrayList<Pizza>(){{
-		add(new Pizza(1, "Sea pizza 1", Type.SEA));
-		add(new Pizza(2, "Regular pizza 1", Type.REGULAR));
-		add(new Pizza(3, "Vegeterian pizza 1", Type.VEGETERIAN));
-	}};
+	private List<Pizza> pizzasList = new ArrayList<Pizza>();
 
+	@PostConstruct
+	public void cookPizzas() {
+		pizzasList.add(new Pizza(1, "Sea pizza 1", Type.SEA));
+		pizzasList.add(new Pizza(2, "Regular pizza 1", Type.REGULAR));
+		pizzasList.add(new Pizza(3, "Vegeterian pizza 1", Type.VEGETERIAN));
+	}
+	
 	@Override
 	public Pizza getPizzaByID(Integer id) {
 		for(Pizza p : pizzasList) {
