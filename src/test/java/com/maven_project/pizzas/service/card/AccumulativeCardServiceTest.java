8f1e36a922aaf306e56f4386a472f962752d4dc3
Flower63 <cardreader8000@gmail.com>
2016-04-06 12:55:16 +0300

    Few tests added

---
+++ src/test/java/com/maven_project/pizzas/service/card/AccumulativeCardServiceTest.java
@@ -0,0 +1,17 @@
+package com.maven_project.pizzas.service.card;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Dennis on 4/6/2016.
+ */
+public class AccumulativeCardServiceTest extends TestCase {
+
+    public void testFindCard() throws Exception {
+
+    }
+
+    public void testRefillCard() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/maven_project/pizzas/service/discount/DiscountServiceTest.java
@@ -0,0 +1,13 @@
+package com.maven_project.pizzas.service.discount;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Dennis on 4/6/2016.
+ */
+public class DiscountServiceTest extends TestCase {
+
+    public void testCountDiscount() throws Exception {
+
+    }
+}
