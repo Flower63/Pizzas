597c35305dec0ba6a64f44622c460302bcf89c22
Antonio <Antonio@10.17.161.66>
2016-04-08 16:59:45 +0300

    Factory method added take two

---
+++ src/main/java/com/maven_project/pizzas/CreatePizzaService.java
@@ -0,0 +1,10 @@
+package com.maven_project.pizzas;
+
+import com.maven_project.pizzas.domain.Pizza;
+import com.maven_project.pizzas.domain.Pizza.Type;
+
+public class CreatePizzaService {
+	public Pizza createPizza(Integer id, String name, Type type, Double cost) {
+		return new Pizza(id, name, type, cost);
+	}
+}
